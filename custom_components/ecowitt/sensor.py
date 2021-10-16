53ed6b894175e2a9f654e3ff1dfbc752f45bdd7d
garbled1 <root@garbled.net>
2021-10-16 06:44:27 -0700

    change to native_value

--- custom_components/ecowitt/sensor.py
+++ custom_components/ecowitt/sensor.py
@@ -46,7 +46,7 @@ class EcowittSensor(EcowittEntity):
         self._sc = sc
 
     @property
-    def state(self):
+    def native_value(self):
         """Return the state of the sensor."""
         if self._key in self._ws.last_values:
             # The lightning time is reported in UTC, hooray.
@@ -65,7 +65,7 @@ class EcowittSensor(EcowittEntity):
         return STATE_UNKNOWN
 
     @property
-    def unit_of_measurement(self):
+    def native_unit_of_measurement(self):
         """Return the unit of measurement."""
         return self._uom
 
